cf1853c0c4e32dc029f20521d454cb3a5d0d521a
Daulatram Kushwah <daulat.java51@gmail.com>
2025-08-08 09:51:51 +0530

    LibraryServiceImp.java

--- LibraryManagementSystem/src/com/cb/daulat/service/imp/LibraryServiceImp.java
+++ LibraryManagementSystem/src/com/cb/daulat/service/imp/LibraryServiceImp.java
@@ -10,30 +10,15 @@ import com.cb.daulat.entity.User;
 public class LibraryServiceImp implements LibraryService {
 	UserServiceImp userImp = new UserServiceImp();
 	private List<Book> books;
-    private List<User> users;
+    
     public LibraryServiceImp() {
-        books = new ArrayList<>();
-        users = new ArrayList<>();
+        books = new ArrayList<>();  
     }
 
 	public void addBook(Book book) {
         books.add(book);
     }
-
-    public void addUser(User user) {
-        users.add(user);
-    }
-    
-    public void viewUsers() {
-        if (users.isEmpty()) {
-            System.out.println("No users in library.");
-        } else {
-            for (User u : users) {
-                System.out.println(u);
-            }
-        }
-    }
-
+   
     public void showBooks() {
     	if (books.isEmpty()) {
             System.out.println("No books in library.");
@@ -44,25 +29,17 @@ public class LibraryServiceImp implements LibraryService {
             
         }
     	
-    	
     }
 
-    public Book findBookById(int bookId) {
+    public Book findBookById(String bookId) {
         for (Book b : books) {
-            if (b.getId() == bookId) return b;
+            if (b.getId().equals(bookId)) return b;
         }
         return null;
     }
 
-    public User findUserById(int userId) {
-        for (User u : users) {
-            if (u.getId() == userId) return u;
-        }
-        return null;
-    }
-
-    public void issueBook(int userId, int bookId) {
-        User user = findUserById(userId);
+    public void issueBook(int userId, String bookId) {
+        User user = userImp.findUserById(userId);
         Book book = findBookById(bookId);
 
         if (user != null && book != null && !book.isIssued()) {
@@ -73,8 +50,8 @@ public class LibraryServiceImp implements LibraryService {
         }
     }
 
-    public void returnBook(int userId, int bookId) {
-        User user = findUserById(userId);
+    public void returnBook(int userId, String bookId) {
+        User user = userImp.findUserById(userId);
         Book book = findBookById(bookId);
 
         if (user != null && book != null && book.isIssued()) {
