7768a046a0b7ebc7c4f2bf75813cd9b2b5465ad4
Daulatram Kushwah <daulat.java51@gmail.com>
2025-08-08 09:52:34 +0530

    UserServiceImp.java

--- LibraryManagementSystem/src/com/cb/daulat/service/imp/UserServiceImp.java
+++ LibraryManagementSystem/src/com/cb/daulat/service/imp/UserServiceImp.java
@@ -1,6 +1,8 @@
 package com.cb.daulat.service.imp;
 
 import com.cb.daulat.entity.Book;
+import com.cb.daulat.entity.User;
+
 import java.util.*;
 
 
@@ -9,11 +11,33 @@ import com.cb.daulat.service.UserService;
 public class UserServiceImp implements UserService{
 	
 	private List<Book> borrowedBooks = new ArrayList<>();
+	private List<User> users = new ArrayList<>();
+	
+	public void addUser(User user) {
+        users.add(user);
+    }
+	
+	public void viewUsers() {
+        if (users.isEmpty()) {
+            System.out.println("No users in library.");
+        } else {
+            for (User u : users) {
+                System.out.println(u);
+            }
+        }
+    }
+	public User findUserById(int userId) {
+        for (User u : users) {
+            if (u.getId() == userId) return u;
+        }
+        return null;
+    }
 	
 	@Override
 	public void borrowBook(Book book) {
 		borrowedBooks.add(book);
         book.issueBook();
+        System.out.println("Book borrowed successfuly");
 		
 	}
 
@@ -21,8 +45,10 @@ public class UserServiceImp implements UserService{
 	public boolean returnBook(Book book) {
 		if (borrowedBooks.remove(book)) {
             book.returnBook();
+            System.out.println("Borrowed book returned successfuly");
             return true;
         }
+		System.out.println("This book is not borrowed");
         return false;
     }
 	
